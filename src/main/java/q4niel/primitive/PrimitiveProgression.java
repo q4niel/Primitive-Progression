9fd9c5426efcb723637295864614bf0ae721c7d3
q4niel <github.obscure938@passmail.net>
2024-11-27 09:33:24 +0100

    Fabric Mod Template

---
+++ src/main/java/q4niel/primitive/PrimitiveProgression.java
@@ -0,0 +1,14 @@
+package q4niel.primitive;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrimitiveProgression implements ModInitializer {
+	public static final String MOD_ID = "primitive";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/q4niel/primitive/PrimitiveProgressionDataGenerator.java
@@ -0,0 +1,9 @@
+package q4niel.primitive;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PrimitiveProgressionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
